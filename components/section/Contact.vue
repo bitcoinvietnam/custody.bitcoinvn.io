db95b892eb0d8ad8fdc79f899a57da6b9bc223d6
rowenslee <rowens.lee@gmail.com>
2023-09-19 09:30:50 +0700

    Add Telegram and Phone inputs

--- components/section/Contact.vue
+++ components/section/Contact.vue
@@ -1,28 +1,36 @@
 <template>
   <section id="contact" class="text-white bg-dark-blue/30 section">
-    <div class="w-full md:w-3/6 section-ctn ">
+    <div class="w-full overflow-y-auto md:w-3/6 section-ctn">
       <div class="text-gold-500 header">
         Contact Us
       </div>
       <div class="paragraph">
-        <form class="flex flex-col">
-          <label for="email" class="pt-3 pb-3 font-bold">Your Email Address:</label>
-          <input type="text" v-model="email" id="email"
-            class="w-full p-2 text-base font-bold text-center text-white rounded-lg bg-gray-100/50 md:p-3 md:text-lg" />
-          <div v-if="!validEmail" class="pt-1 text-red-500">
+        <form class="flex flex-col space-y-3">
+          <input type="text" v-model="email" id="email" placeholder="Enter Your Email Address"
+            class="w-full p-2 text-base font-bold text-center text-white rounded-lg placeholder:text-gray-300 placeholder:font-normal placeholder:text-sm bg-gray-100/50 md:p-3 md:text-lg" />
+          <div v-if="!isValidEmail" class="pt-1 text-red-500">
             Please enter a valid email address.
           </div>
-          <label for="message" class="pt-6 pb-3 font-bold">Your Message:</label>
-          <textarea id="message" v-model="message" rows="6"
-            class="w-full p-2 text-sm text-gray-200 rounded-lg bg-gray-300/50 md:p-3 md:text-sm" />
-        </form>
-        <div v-if="successResp" class="pt-5 font-bold tracking-wide text-center text-green-500">
-          <div>
-            Thank you for contacting us.
+          <input type="tel" id="phone" v-model="phone" placeholder="Your Telephone Number"
+            class="w-full p-2 text-base font-bold text-center text-white rounded-lg placeholder:text-gray-300 placeholder:font-normal placeholder:text-sm bg-gray-100/50 md:p-3 md:text-lg" />
+          <div v-if="!isValidPhone" class="text-red-500">
+            Please enter a valid phone number.
           </div>
-          <div>
-            We will reach back to you soon!
-
+          <input type="text" v-model="telegram" id="telegram" placeholder="Your Telegram Handle"
+            class="w-full p-2 text-base font-bold text-center text-white rounded-lg placeholder:text-gray-300 placeholder:font-normal placeholder:text-sm bg-gray-100/50 md:p-3 md:text-lg" />
+          <div v-if="!isValidTelegram" class="text-red-500">
+            Please enter a valid Telegram handle.
+          </div>
+          <textarea id="message" v-model="message" rows="6" placeholder="Your Message"
+            class="w-full p-2 text-sm text-gray-200 rounded-lg placeholder:text-gray-300 placeholder:font-normal placeholder:text-sm bg-gray-300/50 md:p-3 md:text-sm" />
+        </form>
+        <div v-if="successResp" class="text-green-600">
+          <div class="font-bold">
+            Thank you for contacting us. We will get back to you shortly via the provided email.
+          </div>
+          <div class="text-gray-500">You can safely close this tab now.</div>
+          <div class="text-sm italic">
+            We reply within the same day - please also check your spam folder carefully.
           </div>
         </div>
       </div>
@@ -39,8 +47,12 @@ export default {
     return {
       email: '',
       message: '',
+      phone: '',
+      telegram: '',
       endpoint: 'https://formspree.io/f/mvojqwjj',
-      validEmail: true,
+      isValidEmail: true,
+      isValidPhone: true,
+      isValidTelegram: true,
       successResp: false
     }
   },
@@ -55,15 +67,33 @@ export default {
       return false
     },
 
+    validatePhone(phone) {
+      if (!phone) {
+        return false
+      }
+      return true
+    },
+
+    validateTelegram(handle) {
+      if (!handle) {
+        return false
+      }
+      return true
+    },
+
     async submit() {
       this.successResp = false
-      this.validEmail = this.validateEmail(this.email)
-      if (!this.validEmail) {
-        return
+      this.isValidEmail = this.validateEmail(this.email)
+      this.isValidPhone = this.validatePhone(this.phone)
+      this.isValidTelegram = this.validateTelegram(this.telegram)
+      if (!this.isValidEmail || !this.isValidPhone || !this.isValidTelegram) {
+        return false
       }
       const data = {
         email: this.email,
-        message: this.message
+        message: this.message,
+        phone: this.phone,
+        telegram: this.telegram,
       }
       // Formspree side will send an email for confirmation.
       // So we just display a thank you note here
