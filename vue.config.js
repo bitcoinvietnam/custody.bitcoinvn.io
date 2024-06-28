49847de756d00bb200ce3ede485031cb035df099
rowenslee <rowens.lee@gmail.com>
2024-06-28 13:08:43 +0700

    Remove config of base url in vue, use nuxt config instead

--- vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // Set public path for different environments
-  publicPath: process.env.NODE_ENV === 'PROD' ? '/' : process.env.NODE_ENV === 'STAGE' ? '/custody.bitcoinvn.io/' : '/'
-}
